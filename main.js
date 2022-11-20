734a3d2573c4efa4d6d8c53ea041510fccfa70ce
Bhavishyaa Vignesh <67924047+bhavicodey@users.noreply.github.com>
2022-11-19 19:46:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+Webcam.set({
+    width:350,
+    height:250,
+    image_format : 'png',
+    png_quality:100
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+     
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+ 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/GkSH_W5Xd/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+     
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    object_name = document.getElementById("result_object_name").innerHTML = results[0].label;
+  }
+  if (object_name == "Australia") {
+    location.href = 'australia.html';
+  }
+  if (object_name == "India") {
+    location.href = 'india.html';
+
+  }
+}
